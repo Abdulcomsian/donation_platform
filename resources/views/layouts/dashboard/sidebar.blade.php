0f43cb5d7105f6556f459a9cf982104555832dde
Shahbaz <rajashayzee@yahoo.com>
2024-01-08 19:23:33 +0500

    design changes

--- resources/views/layouts/dashboard/sidebar.blade.php
+++ resources/views/layouts/dashboard/sidebar.blade.php
@@ -161,7 +161,7 @@
                             <span>Membership</span>
                         </a>
                     </li>
-                    <li class="menu-item {{Request::is('events') ? 'active' : ''}}">
+                    <li class="menu-item {{Request::is('events') || Request::is('events/*') ? 'active' : ''}}">
                         <div class="active-icon">
                             <svg xmlns="http://www.w3.org/2000/svg" width="8" height="23" viewBox="0 0 8 23"
                                 fill="none">
